b28c8756d80b79ca0b5ea450ff92f0096acab8ee
2777maxim <2777maxim@mail.ru>
2025-08-05 16:08:26 +0300

    правки

--- stats/analyzer/src/main/java/ru/practicum/analyzer/AnalyzerApplication.java
+++ stats/analyzer/src/main/java/ru/practicum/analyzer/AnalyzerApplication.java
@@ -2,7 +2,9 @@ package ru.practicum.analyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AnalyzerApplication {
     public static void main(String[] args) {
--- stats/collector/src/main/java/ru/practicum/collector/CollectorApplication.java
+++ stats/collector/src/main/java/ru/practicum/collector/CollectorApplication.java
@@ -2,7 +2,9 @@ package ru.practicum.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CollectorApplication {
     public static void main(String[] args) {
