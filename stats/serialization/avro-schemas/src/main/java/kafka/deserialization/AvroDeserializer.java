7f9445ba9d39c4897c8d21d073b32cbc70e833ad
2777maxim <2777maxim@mail.ru>
2025-08-06 21:32:56 +0300

    правки codestyle

--- stats/serialization/avro-schemas/src/main/java/kafka/deserialization/AvroDeserializer.java
+++ stats/serialization/avro-schemas/src/main/java/kafka/deserialization/AvroDeserializer.java
@@ -33,7 +33,7 @@ public class AvroDeserializer<T extends SpecificRecordBase> implements Deseriali
             }
             return null;
         } catch (Exception e) {
-            throw new SerializationException("Unable to deserialize message from topic [" + topic + "]");
+            throw new SerializationException("Unable to deserialize message from topic [" + topic + "]", e);
         }
     }
 
